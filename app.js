330e606420414f6c6696841a01bfb4f12c84a9be
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2020-12-18 14:26:44 +0530

    fixed server error

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 function constructURL(input){
     return serverURL+ "?"+ "text="+ input;
 }
+
 function errorHandler(error){
     console.log("error occured",error);
     alert("something wrong with server please try again later");
 }
 
 function clickHandler(){
-var inputText = txtInput.value;
+ var inputText = txtInput.value;
 
-fetch(constructURL(inputText))
-.then(response => response.json())
-.then(json => {
-var translatedText= json.contents.translated;
-outputDiv.innerText= translatedText;
+  fetch(constructURL(inputText))
+   .then(response => response.json())
+    .then(json => {
+      var translatedText= json.contents.translated;
+       outputDiv.innerText= translatedText;
 
 }
   )
