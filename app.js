4385ad7deee2bd3a8f1253e65195dbc6b46157da
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-02 15:52:17 +0530

    reafctored with es6

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var btnTranslate = document.querySelector("#btn");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+const btnTranslate = document.querySelector("#btn");
+const txtInput = document.querySelector("#txt-input");
+const outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+const serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
+const getTranslationURL = (input) => `${serverURL}?text=${input}`;
 
+const errorHandler = (error) => {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+};
+const showOutput = (data) => {
+  outputDiv.innerText = data; // output
+};
 
+const clickHandler = () => {
+  btnTranslate.textContent = "Translating......";
+  const inputText = txtInput.value; // taking input
 
-function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + input;
-}
-
-function errorHandler(error) {
-    console.log("error occured",error);
-    alert("something wrong with server! try again after some time");
-}
-
-
-function clickHandler() {
-    var inputText = txtInput.value; // taking input
-
-    // calling server for processing
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
-           })
-        
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      setTimeout(() => {
+        btnTranslate.textContent = "Translate";
+      }, 2000);
+      showOutput(json.contents.translated);
+    })
+    .catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click", clickHandler);
